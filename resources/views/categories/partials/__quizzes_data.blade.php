10105e8199e53925616476d47cf96a35f5d2d15c
balagtasangelo <79126040+balagtasangelo@users.noreply.github.com>
2021-04-05 11:58:20 +0800

    [SELS-TASK] Student: Apply integration for Categories Page
    Create color background for cards to white.

--- resources/views/categories/partials/__quizzes_data.blade.php
+++ resources/views/categories/partials/__quizzes_data.blade.php
@@ -1,6 +1,6 @@
 <div class="flex flex-wrap content-start justify-content-center">
     @foreach ($quizzes as $quiz)
-       <div class="w-2/5 border border-black m-4 p-4">
+       <div class="w-2/5 border border-black m-4 p-4 bg-white">
           <div class="font-bold m-2">{{ $quiz->title }}</div>
 
           <div class="m-2">
